7c63f592a3ac0b2f2a7e463ea27e11aae92650db
Saadat Taaseen <login@saadt.dev>
2024-02-25 02:36:08 -0500

    working notification permission

---
+++ src/helpers/NotificationHelper.js
@@ -0,0 +1,37 @@
+import messaging from '@react-native-firebase/messaging';
+import {PermissionsAndroid} from 'react-native';
+
+class NotificationHelper {
+  constructor() {
+    console.log(' Notification helper');
+  }
+
+  requestNotificationPermissionForiOS = async () => {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) {
+      console.log('Authorization status:', authStatus);
+    }
+  };
+
+  requestNotificationPermissionForAndroid = async () => {
+    try {
+      let granted = PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
+      );
+      if (granted == PermissionsAndroid.RESULTS.GRANTED) {
+        alert('Notifications Permission Granted ');
+      } else {
+        alert('Notifications Permission Not Granted');
+      }
+    } catch (err) {
+      console.log('Notification Permission Error ==>', err);
+      alert('Error With Notification Permission');
+    }
+  };
+}
+
+export default new NotificationHelper();
--- src/screens/HomeScreen/index.jsx
+++ src/screens/HomeScreen/index.jsx
@@ -1,24 +1,50 @@
-import {View, Text, Button} from 'react-native';
-import React from 'react';
+import {View, Text, Button, Platform, Alert} from 'react-native';
+import React, {useEffect, useState} from 'react';
 import {useNavigation} from '@react-navigation/native';
 import {firebaseLogOut} from 'src/helpers/FirebaseHelper';
+import NotificationHelper from 'src/helpers/NotificationHelper';
+import messaging from '@react-native-firebase/messaging';
+
 const HomeScreen = () => {
-  const navigation = useNavigation();
+  // const navigation = useNavigation();
+  const [notificationsOn, setNotificationsOn] = useState(false);
+  const askForPermission = () => {
+    Platform.OS == 'ios'
+      ? NotificationHelper.requestNotificationPermissionForiOS()
+      : NotificationHelper.requestNotificationPermissionForAndroid();
+  };
+
+  useEffect(() => {
+    notificationsOn && askForPermission();
+  }, [notificationsOn]);
+
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+    return unsubscribe;
+  }, []);
 
   return (
     <View>
+      <Button
+        title="Activate Notification Permissions"
+        onPress={() => {
+          setNotificationsOn(!notificationsOn);
+        }}
+      />
       <Button
         title="Log Out"
         onPress={() => {
           firebaseLogOut();
         }}
       />
-      <Button
+      {/* <Button
         title="Fetch"
         onPress={() => {
           navigation.navigate('Fetch');
         }}
-      />
+      /> */}
     </View>
   );
 };
